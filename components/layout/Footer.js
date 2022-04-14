1dcbe4a0254af8a637cb4b238e2536d0901f47b8
nathalieacosta <nathalieacosta@college.harvard.edu>
2022-04-13 21:31:23 -0400

    fix github img

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,24 +1,27 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Image from "next/image";
+import githubLogo from "../public/github.png";
 
 import styles from "../../styles/Footer.module.css";
 
 const Footer = () => {
-    return (
-            <Container className={styles.footer} fluid>
-                <Row>
-                    <Col className={styles.links}>
-                        <a href="https://github.com/nathalieacosta/cloudy"><Image src={"/../public/github.png"} width={30} height={30}></Image></a>
-                    </Col>
-                    <Col className={styles.nametag}>
-                        <a>made with ♡ by nathalie acosta</a>
-                    </Col>
-                    <Col className={styles.links}>
-                        <a href="mailto: nathalieacosta@college.harvard.edu">contact me</a>
-                    </Col>
-                </Row>
-            </Container>
-    )
-}
+  return (
+    <Container className={styles.footer} fluid>
+      <Row>
+        <Col className={styles.links}>
+          <a href="https://github.com/nathalieacosta/cloudy">
+            <Image src={githubLogo} width={30} height={30}></Image>
+          </a>
+        </Col>
+        <Col className={styles.nametag}>
+          <a>made with ♡ by nathalie acosta</a>
+        </Col>
+        <Col className={styles.links}>
+          <a href="mailto: nathalieacosta@college.harvard.edu">contact me</a>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Footer;
